1af89cb4cd76ef7f1c03463c4cabbe5d3f63fcb8
Julien Wilhelm <contact@awebsome.fr>
2022-06-13 14:54:01 +0200

    Adds project infos.

--- src/components/About.svelte
+++ src/components/About.svelte
@@ -1,3 +1,7 @@
 <section id="a-propos" class="page-anchor">
     <h2>À propos</h2>
+    <p>Cette WebExtension, commandée par <a href="https://www.numerique.gouv.fr/dinum/" target="_blank">la Direction Interministérielle du Numérique</a> (DINUM) et réalisée par <a href="https://temesis.com" target="_blank">Temesis</a> facilite l'auto-évaluation par vos soins de la conformité de votre service numérique selon le <a href="https://ecoresponsable.numerique.gouv.fr/publications/referentiel-general-ecoconception/" target="_blank">Référentien Général d'Écoconception de Services Numériques</a> (RGESN).</p>
+    <p>Ce référentiel est sous <a href="https://www.etalab.gouv.fr/licence-ouverte-open-licence" target="_blank">licence ouverte Etalab v2</a>.<br/>
+    Cette extension, sous <a href="https://fr.wikipedia.org/wiki/Licence_MIT" target="_blank">licence MIT</a>.</p>
+    <p>Pour contribuer à l'amélioration de cet outil (report de bugs, pistes d'amélioration, etc.), visitez <a href="https://github.com/DISIC/ecodesign-addon" target="_blank">le dépôt GitHub officiel du projet</a>.
 </section>
