b7392082f4de74391633c9ff3d436b7d120fcedd
Julien Wilhelm <contact@awebsome.fr>
2023-01-12 14:36:36 +0100

    Refactoring.

--- src/modules/module.mjs
+++ src/modules/module.mjs
@@ -1,5 +1,5 @@
-export const webAppMode = true; // Makes NumÉcoDiag able to be served as webApp and makes debugging easier (entry point: /public/index.html) 
-export const env = window.browser === undefined ? window.chrome : window.browser; // Makes NumÉcoDiag cross browser as webExtension
+export const webAppMode = false; // Makes NumÉcoDiag able to be served as webApp to make debugging easier (server required)
+export const env = window.browser === undefined ? window.chrome : window.browser; // Makes NumÉcoDiag cross browser as webExtension (no server required)
 
 export const setLocalData = (name, value) => {
     return new Promise(
@@ -13,8 +13,8 @@ export const setLocalData = (name, value) => {
                 resolve();
             }
         }
-    )
-}
+    );
+};
 
 export const getLocalData = name => {
     return new Promise(
@@ -27,38 +27,48 @@ export const getLocalData = name => {
                 resolve(localStorage.getItem(name));
             }
         }
-    )		
-}
+    );	
+};
 
+export const getAudits = () => {
+    return new Promise(
+        (resolve ,reject) => {
+            getLocalData('audits')
+                .then((data) => {
+                    if((data !== null && data !== undefined)) {
+                        resolve(data);
+                    }
+                    else {
+                        reject('Absence d\'historique');
+                    }
+                }
+            );
+        }
+    );
+};
 
-export function getAudits() {
-    return new Promise((resolve ,reject) => {
-        getLocalData('audits').then((data) =>
-            (data !== null && data !== undefined) ? resolve(data): reject('Absence d\'historique')
-        );
-    });
-}
-
-export function getRGESN(versionToLoad) {
-    return new Promise((resolve, reject) => {
-        fetch(`./rgesn/${versionToLoad}.json`)
-            .then((resp) => { 
-                if(resp.ok) {
-                    resp.text()
-                        .then((data) => {
-                            try {
-                                resolve(JSON.parse(data));
-                            }
-                            catch(e) {
-                                reject('Lecture du référentiel impossible');
+export const getRGESN = (versionToLoad) => {
+    return new Promise(
+        (resolve, reject) => {
+            fetch(`./rgesn/${versionToLoad}.json`)
+                .then((resp) => { 
+                    if(resp.ok) {
+                        resp.text()
+                            .then((data) => {
+                                try {
+                                    resolve(JSON.parse(data));
+                                }
+                                catch(e) {
+                                    reject('Lecture du référentiel impossible');
+                                }	
                             }	
-                        }	
-                    )
+                        );
+                    }
+                    else {
+                        reject('Ouverture du référentiel impossible');
+                    }
                 }
-                else {
-                    reject('Ouverture du référentiel impossible');
-                }
-            }
-        )
-    });	
-}
+            );
+        }
+    );	
+};
